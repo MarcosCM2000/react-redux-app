245acca49e993aced5ecaad912821ec1e092751b
Marcos Calderon <mcalderonig@gmail.com>
2023-04-23 18:17:11 -0600

    providing the store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Import provider
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+// Import store
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
